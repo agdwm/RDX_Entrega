a91ca5911d8a20d04375ce9d6a67906f212c2281
agdwm <almudenaguerraswm@gmail.com>
2018-05-23 18:45:49 +0200

    Finished functionality menu and changed the names or the variables related to the colors

--- src/js/presentation/pushableManager.js
+++ src/js/presentation/pushableManager.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const $ = require('jquery');
 
 export default class PushableManager {
@@ -11,50 +13,47 @@ export default class PushableManager {
 	}
 
 	init () {
+		self = this;
 		this.setupClickEventHandler();
 		this.setupMatchEventHandler();
 	}
 
 	setupClickEventHandler() {
-		this.btnMenu.on('click', (e) => {
-			this.switchButton(this.btnMenu);
-			this.switchHeaderContent();
-			this.switchPushableContent();
-			return false;
+		// On click in "logo" or in "btnMenu", trigger the following functions
+		this.logo.add(this.btnMenu).on('click', (e) => {
+			if (!this.mq.matches) {
+				this.switchButton();
+				this.switchHeaderContent();
+				this.switchPushableContent();
+				return false;
+			}
 		});
-
-		this.logo.on('click', (e) => {
-			this.switchButton(this.btnMenu);
-			this.switchHeaderContent();
-			this.switchPushableContent();
-			return false;
-		})
 	}
 
 	setupMatchEventHandler() {
 		if (matchMedia) {
-			this.mq.addListener(this.toggleState);
-			this.toggleState(this.mq);
+			this.mq.addListener(this.closeSideMenu);
+			this.closeSideMenu(this.mq);
 		}
 	}
 
-	switchButton(btn) {
-		btn.toggleClass('active');
+	closeSideMenu (mq) {
+		// if window width is at least 768px
+		if (mq.matches) {
+			self.btnMenu.removeClass('active');
+			self.headerContent.removeClass('active');
+			self.pushableContent.removeClass('active');
+			return false;
+		}
+	}
+
+	switchButton() {
+		this.btnMenu.toggleClass('active');
 	}
 	switchHeaderContent(){
 		this.headerContent.toggleClass('active');
 	}
 	switchPushableContent(){
 		this.pushableContent.toggleClass('active');
-	}
-
-	// https://www.sitepoint.com/javascript-media-queries/
-	toggleState(mq) {
-		if (mq.matches) {
-			$('.footer').css({'background-color': 'red'});
-		}else{
-			$('.footer').css({'background-color': 'green'});
-		}
-		console.log("CLICK");
 	}
 }
